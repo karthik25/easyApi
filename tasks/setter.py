1ceb53ccc24f0389061efa973b7249704db49766
Karthik Anant <karthika25@gmail.com>
2020-10-19 22:12:54 -0400

    updated setter to process various token types

--- tasks/setter.py
+++ tasks/setter.py
@@ -1,7 +1,12 @@
+import urllib.request
+import json
+import urllib.parse
+
+
 class Setter:
     key_value_dictionary = {
         "access_token": "",
-        "token_url": "",
+        "token_endpoint": "",
         "client_id": "",
         "client_secret": "",
         "grant_type": "client_credentials",
@@ -34,18 +39,37 @@ class Setter:
 
     @staticmethod
     def generate_access_token():
-        pass
+        full_url = Setter.key_value_dictionary["token_endpoint"]
+
+        post_data = {
+            "grant_type": Setter.key_value_dictionary["grant_type"],
+            "scope": Setter.key_value_dictionary["scope"],
+            "client_id": Setter.key_value_dictionary["client_id"],
+            "client_secret": Setter.key_value_dictionary["client_secret"]
+        }
+
+        encoded_data = urllib.parse.urlencode(post_data).encode()
+        req = urllib.request.Request(full_url, data=encoded_data)
+        token_response = urllib.request.urlopen(req)
+
+        response_content = token_response.read()
+        parsed_response = json.loads(response_content)
+        access_token = parsed_response["access_token"]
+        return access_token
 
     @staticmethod
     def get_api_headers():
         if Setter.key_value_dictionary["token_type"] == "None":
+            print("Setter: token type is None - returning empty headers")
             return {}
 
         if Setter.key_value_dictionary["token_type"] == "Bearer":
             if Setter.key_value_dictionary["access_token"] is not None or Setter.key_value_dictionary["access_token"] != "":
+                print("Setter: using the access token")
                 return { "Authorization", "Bearer {0}".format(Setter.key_value_dictionary["access_token"])}
             else:
+                print("Setter: genreating the access token")
                 accessToken = Setter.generate_access_token()
                 return {"Authorization", "Bearer {0}".format(accessToken)}
 
-        raise Exception("do not know how to handle {0}".format(Setter.key_value_dictionary["token_type"]))
+        raise Exception("Do not know how to handle {0}".format(Setter.key_value_dictionary["token_type"]))
