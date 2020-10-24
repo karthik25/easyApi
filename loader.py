97462b725a893e81ea1d68fd88a6c8584d400765
Karthik Anant <karthika25@gmail.com>
2020-10-24 12:35:54 -0400

    a faster loader

--- loader.py
+++ loader.py
@@ -12,18 +12,29 @@ from tasks.help import Help
 
 
 class Loader:
+    task_dictionary = {
+
+    }
+
     def __init__(self):
         pass
 
     @staticmethod
     def get_by_identifier(identifier):
-        all_cls = globals()
-        for cls in all_cls:
-            current_cls = globals()[cls]
-            if inspect.isclass(current_cls) and issubclass(current_cls, Task):
-                handles = getattr(current_cls, "handles", None)
-                if handles is not None:
-                    handles_value = handles()
-                    if handles_value == identifier:
-                        return current_cls()
+        Loader.populate_task_dictionary()
+        if identifier in Loader.task_dictionary.keys():
+            type = Loader.task_dictionary[identifier]
+            return type()
         return None
+
+    @staticmethod
+    def populate_task_dictionary():
+        if len(Loader.task_dictionary.keys()) == 0:
+            all_cls = globals()
+            for cls in all_cls:
+                current_cls = globals()[cls]
+                if inspect.isclass(current_cls) and issubclass(current_cls, Task):
+                    handles = getattr(current_cls, "handles", None)
+                    if handles is not None:
+                        handles_value = handles()
+                        Loader.task_dictionary[handles_value] = current_cls
